c74fe0b80af2d51ab2ff626b3c5038fcbe0b0186
Spanos <nickspanosf1@gmail.com>
2024-11-15 21:16:46 +0200

    add loader for form submission

--- src/components/CtaSection/CtaSection.js
+++ src/components/CtaSection/CtaSection.js
@@ -2,9 +2,10 @@ import React, {useState} from 'react';
 import './CtaSection.css';
 
 export function CtaSection() {
-    const [disabled, setDisabled] = useState(false);
+    const [disabled, setDisabled] = useState(true);
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
 
     function handleEmailChange(event) {
         setEmail(event.target.value);
@@ -13,21 +14,42 @@ export function CtaSection() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        try {
-            const response = await fetch('https://nickport.com/save_email.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email }),
-            });
+        if (!isValidEmail(email)) {
+            setMessage('Please enter a valid email address.');
+        }else {
+            setLoading(true);
+            try {
+                const response = await fetch('https://nickport.com/save_email.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email }),
+                });
+                const result = await response.json();
+                setMessage(result.message);
 
-            const result = await response.json();
-            setMessage(result.message);
-        } catch (error) {
-            setMessage('An error occurred. Please try again.');
+
+            } catch (error) {
+                setMessage('An error occurred. Please try again.');
+            }
+
+            setTimeout(() => {
+                setLoading(false);
+                setEmail('');
+                setDisabled(true);
+                setMessage('');
+            },5000)
         }
+
     };
+    function isValidEmail(email) {
+        // Regular expression to validate an email address
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailRegex.test(email);
+    }
+
+
 
     return (
         <section className='cta_section'>
@@ -36,10 +58,15 @@ export function CtaSection() {
                 <p className='cta_description'>Συμπληρώστε το email σας παρακάτω για να μάθετε πρώτοι τα νέα και τις
                     λειτουργίες του <span>NutriScan</span> </p>
                 <form className='cta_form' onSubmit={handleSubmit}>
-                    <input type='email' onChange={handleEmailChange} placeholder='Το email σας'/>
+                    <input type='email' onChange={handleEmailChange} value={email} placeholder='Το email σας'/>
                     <button  disabled={disabled}>Εγγραφή</button>
                 </form>
                 {message && <p className='returnMessage'> {message}</p>}
+                {loading && <div className="d-flex justify-content-center align-items-center" >
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>}
             </div>
         </section>
     );
